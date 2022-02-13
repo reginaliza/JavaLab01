75bdc56166f527dbc8a51f374ca1c012e3b40278
CJ Corona <98691443+cjbcrown1112@users.noreply.github.com>
2022-02-13 12:09:45 +0800

    Update PrintTypes.java
    Ayaw Gumana T.T

--- src/javaLab01/PrintTypes.java
+++ src/javaLab01/PrintTypes.java
@@ -15,21 +15,35 @@ public class PrintTypes extends transactionProcessing {
             System.out.println("\n1. Money Transfer" + "\n2. Bills Payment" +
                     "\n3. Buy Load" + "\n4. Add Account Credits" + "\n5. Add Game Credits" + "\n\nPick a Transaction: ");
             Scanner type = new Scanner(System.in);
-            int choice = type.nextInt();
-            transactionProcessing tpObj = new transactionProcessing();
+            int pick = type.nextInt();
 
 
-            switch (choice) {
-                case 1:
-                    tpObj.addMoneyTransfer();
-                case 2:
-                    tpObj.addBillsPayment();
-                case 3:
-                    tpObj.addBuyLoad();
-                case 4:
-                    tpObj.addAddAccountCredits();
-                case 5:
-                    tpObj.addAddGameCredits();
+            switch (pick) {
+                case 1: {
+                    transactionProcessing mf = new transactionProcessing();
+                    mf.addMoneyTransfer();
+                    return;
+                }
+                case 2: {
+                    transactionProcessing bp = new transactionProcessing();
+                    bp.addBillsPayment();
+                    return;
+                }
+                case 3: {
+                    transactionProcessing bl = new transactionProcessing();
+                    bl.addBuyLoad();
+                    return;
+                }
+                case 4: {
+                    transactionProcessing aac = new transactionProcessing();
+                    aac.addAddAccountCredits();
+                    return;
+                }
+                case 5: {
+                    transactionProcessing agc = new transactionProcessing();
+                    agc.addAddGameCredits();
+                    return;
+                }
             }
         }
 
